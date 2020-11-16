c48de2a1a715334a5e7526fc064a0eb6d9bc29e4
Veronica Nixon <vsnixon@gmail.com>
2020-11-16 11:58:48 -0700

    Adds start and start_end routes with corresponding start_temps and start_end_temps functions to app.py. All endpoints are working, yipeegit add .!

--- app.py
+++ app.py
@@ -13,14 +13,12 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 # Create db engine for sqlite db.
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # Create inspector object to view tables and columns in sqlite db.
 inspector = inspect(engine)
-# Start SQLAlchemy query session for precipitation analysis.
-session = Session(engine)
 # Create Pandas connection for temperature analysis.
 conn = engine.connect()
 # Read (reflect) sqlite db into SQLAlchemy Base object.
@@ -33,31 +31,23 @@ Measurement = Base.classes.measurement
 # This function, called `start_temps` accepts a start date in the format '%Y-%m-%d' 
 # and returns the max, min, and average temperatures for all dates in the Measurement
 # table since the start date.
-def start_temps(start_date):
-    """
-    Args:
-        start_date (string): A date string in the format %Y-%m-%d
-        end_date (string): A date string in the format %Y-%m-%d   
-    Returns:
-        TMAX, TMIN, and TAVG
-    """
-    return session.query(func.max(Measurement.tobs), func.min(Measurement.tobs), func.avg(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).all()
-    
+def start_temps(date):
+    # Start SQLAlchemy query session.
+    session = Session(engine)
+    return session.query(func.max(Measurement.tobs), func.min(Measurement.tobs),\
+    func.avg(Measurement.tobs)).filter(Measurement.date >= date).all()
+    session.close()
+   
 # This function, called `start_end_temps` accepts start and end dates in the format '%Y-%m-%d' 
 # and returns the max, min, and average temperatures for all observations in the Measurement
 # table that fall within the date range.
 def start_end_temps(start_date, end_date):
-    """
-    Args:
-        start_date (string): A date string in the format %Y-%m-%d
-        end_date (string): A date string in the format %Y-%m-%d
-        
-    Returns:
-        TMAX, TMIN, and TAVG
-    """
-    return session.query(func.max(Measurement.tobs), func.min(Measurement.tobs), func.avg(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    # Start SQLAlchemy query session.
+    session = Session(engine)
+    return session.query(func.max(Measurement.tobs), func.min(Measurement.tobs),\
+    func.avg(Measurement.tobs)).filter(Measurement.date >= start_date).\
+    filter(Measurement.date <= end_date).all()
+    session.close()
 
 # Create app.
 app = Flask(__name__)
@@ -76,17 +66,20 @@ def home():
     http://localhost:5000//api/v1.0/tobs<br><br>\
     The Start endpoint returns the max, min, and average temperature for all days since an input\
     start date (YYYY-MM-DD) in json format:<br>\
-    http://localhost:5000//api/v1.0/?start=2017-07-10<br><br>\
+    http://localhost:5000//api/v1.0/<startdate><br><br>\
     The Start-End endpoint returns the max, min, and average temperature for all days between and\
     including input start and end dates (YYYY-MM-DD) in json format:<br>\
-    http://localhost:5000//api/v1.0/?start=2017-01-01&end=2017-02-01"
+    http://localhost:5000//api/v1.0/<startdate>/<enddate>"
 
 # When user hits the Precipitation route, query the hawaii.sqlite database and return the prcp column.
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Compute date one year before last date present in database.
+    # Start SQLAlchemy query session.
+    session = Session(engine)
     # Query last date in Measurement.
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    session.close()
     # Index into last_date.
     last_date_string = last_date[0]
     # Format string as datetime object.
@@ -111,7 +104,10 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     # Query list of stations from Station table
+    # Start SQLAlchemy query session.
+    session = Session(engine)
     result = session.query(Station.station).all()
+    session.close()
     normal_list = list(np.ravel(result))
     return jsonify(normal_list)   
 
@@ -120,8 +116,11 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def temperature():
     # Compute date one year before last date present in database.
+    # Start SQLAlchemy query session.
+    session = Session(engine)
     # Query last date in database.
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    session.close()
     # Index into last_date.
     last_date_string = last_date[0]
     # Format string as datetime object.
@@ -132,32 +131,39 @@ def temperature():
     # Find the station with the most measurements by grouping the Measurement table by station, 
     # aggregating to count, and sorting descending. This station should provide the most reliable
     # information about Hawaii weather.
+    # Start SQLAlchemy query session.
+    session = Session(engine)
     # Query
     result = session.query(Measurement.station, func.count(Measurement.station)).\
     group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+    session.close()
     # Extract first cell of first row.
     highest_number = result[0][0]
     # For the station with the most observations, query the last year of temperature data.
     results = session.query(Measurement.tobs).filter(Measurement.station == highest_number).\
     filter(Measurement.date >= year_ago).all()
+    session.close()
     normal_list = list(np.ravel(results))
     return jsonify(normal_list)
 
-# When user hits the Start route, 
-@app.route("/api/v1.0/")
-def start():
-    # Call start_temps function with
-    stats = start_temps(start_date)
-    # Convert session.query object into a dictionary.
+# When user hits the Start, return temperature stats based on user input timespan\
+#(start date --> last date in table).
+@app.route("/api/v1.0/<date>")
+def start(date):
+    # Call start_temps function with user input start date to get temp stats.
+    stats = start_temps(date)
+    # Convert session.query object into a "normal" list.
     normal_list = list(np.ravel(stats))
-    return jsonify()
-"""
-# When user hits the Start-End route,
-@app.route("/api/v1.0/")
-def start_end():
+    return jsonify(normal_list)
 
-    normal_list = list(np.ravel(results))
-    return jsonify() """
+# When user hits the Start-End route,
+@app.route("/api/v1.0/<startdate>/<enddate>")
+def start_end(startdate, enddate):
+    # Call start_end_temps function with user input start and end dates to get temp stats.
+    stats = start_end_temps(startdate, enddate)
+    # Convert session.query object into a "normal" list.
+    normal_list = list(np.ravel(stats))
+    return jsonify(normal_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
